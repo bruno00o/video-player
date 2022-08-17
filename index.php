318281f6c5b89a5618b5545a0e303330ec966e91
bruno00o <bseilliebert@outlook.fr>
2022-08-17 10:23:45 +0100

    initial upload

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+function startsWith($haystack, $needle)
+{
+    $length = strlen($needle);
+    return substr($haystack, 0, $length) === $needle;
+}
+function endsWith($haystack, $needle)
+{
+    $length = strlen($needle);
+    if (!$length) {
+        return true;
+    }
+    return substr($haystack, -$length) === $needle;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Video Player</title>
+    <link type="text/css" rel="stylesheet" href="styles.css" media="screen">
+    <link rel="manifest" href="manifest.json">
+    <meta name="apple-mobile-web-app-status-bar" content="default">
+    <meta name="theme-color" content="#071026">
+</head>
+
+<body>
+    <div id="side-bar">
+        <div class="toggle-button" id="toggle-button">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <?php
+        $dir = '';
+        $files1 = scandir($dir);
+        $arrayLength = count($files1);
+        $i = 0;
+        echo '<div class="container">';
+        echo '<div class="select-box">';
+        echo '<div class="options-container">';        
+        while ($i<$arrayLength) {
+            if (startsWith($files1[$i], "SAISON")) {
+                echo '<div class="option">';
+                echo '<input type="radio" class="radio" id="inuput'.$files1[$i].'" name="category" />';
+                echo '<label for="season">'.$files1[$i].'</label>';
+                echo '</div>';
+            }
+            $i++;
+        }
+        
+        echo '</div>';
+        echo '<div class="selected">Choix de la Saison</div>';
+        echo '</div>';
+        echo '</div>';
+        $i=0;
+        while ($i<$arrayLength) {
+            if (startsWith($files1[$i], "SAISON")) {
+                $id = $files1[$i];
+                ${"dir$i"} = '' . $files1[$i];
+                ${"files$i"} = scandir(${"dir$i"});
+                $arrayLength2 = count(${"files$i"});
+                $j=0;
+                echo "<ul id='" . $id . "' style='display: none;'>";
+                while ($j < $arrayLength2) {
+                    if (endsWith(${"files$i"}[$j], 'mp4')) {
+                        echo "<li onclick='change_video(this)'>" . substr_replace(${"files$i"}[$j], '', -4) . "</li>";
+                    }
+                    $j++;
+                };
+                echo "</ul>";
+            }
+            $i++;
+        }
+        ?>
+    </div>
+    <div id="main"></div>
+    <section>
+        <div class="title">
+            <h1>Video Player</h1>
+            <h2 id='title'></h2>
+            <img id="next_button" src="next_button.png" alt="next">
+        </div>
+        <div class="image">
+            <img id="play_button" src="play_button.png" alt="play_button">
+            <img id="play-img" src="img/bg.png" alt="play">
+            <video id="player" playsinline controls></video>
+        </div>
+        
+    </section>
+
+    <!-- <div id="news"></div> -->
+
+    <script src="app.js"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,248 @@
+// Variables
+
+const playImage = document.getElementById('play-img');
+const video = document.querySelector('video');
+const pause_button = document.getElementById('play_button');
+const toggleButton = document.getElementById('toggle-button');
+const sideBar = document.getElementById('side-bar');
+const title = document.getElementById('title');
+const nextButton = document.getElementById('next_button');
+const liList = document.getElementsByTagName('li');
+const scrollDiv = document.querySelector('ul');
+const section = document.querySelector('section');
+const ulList = document.getElementsByTagName('ul');
+const labelList = document.getElementsByTagName('label');
+
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+    optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach(o => {
+    o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector("label").innerHTML;
+        localStorage.setItem('season', selected.innerHTML);
+        optionsContainer.classList.remove("active");
+        let season = (selected.innerHTML);
+        for (let i = 0; i < ulList.length; i++) {
+            ulList[i].style.display = 'none';
+        }
+        let ulSeason = document.getElementById(season);
+        ulSeason.style.display = 'block';
+    });
+});
+
+
+// Met la video en fonction du LocalStorage
+
+let titleStorage = localStorage.getItem('title')
+let videoStorage = localStorage.getItem('video')
+let seasonStorage = localStorage.getItem('season');
+
+
+if (titleStorage == null) {
+    title.innerHTML = liList[0].textContent;
+} else {
+    title.innerHTML = titleStorage;
+};
+
+if (videoStorage == null) {
+    video.setAttribute('src', "videos/" + labelList[0].innerHTML + "/" + liList[0].textContent + ".mp4" + '#t=' + (localStorage.getItem('time') - 5).toString());
+    let elem = liList[0];
+    elem.style.fontWeight = '600';
+    elem.style.color = 'white';
+} else {
+    video.setAttribute('src', videoStorage + '#t=' + (localStorage.getItem('time') - 5).toString());
+    for (let i = 0; i < liList.length; i++) {
+        if (liList[i].textContent == videoStorage.slice(10, videoStorage.search('mp4') - 1)) {
+            let elem = liList[i];
+            elem.style.fontWeight = '600';
+            elem.style.color = 'white';
+            scrollDiv.scroll(0, elem.offsetTop - 100);
+            break;
+        }
+    }
+
+};
+
+if (seasonStorage == null) {
+    selected.innerHTML = labelList[0].textContent;
+    let season = (selected.innerHTML);
+    for (let i = 0; i < ulList.length; i++) {
+        ulList[i].style.display = 'none';
+    }
+    let ulSeason = document.getElementById(season);
+    ulSeason.style.display = 'block';
+} else {
+    selected.innerHTML = seasonStorage;
+    let season = (selected.innerHTML);
+    for (let i = 0; i < ulList.length; i++) {
+        ulList[i].style.display = 'none';
+    }
+    let ulSeason = document.getElementById(season);
+    ulSeason.style.display = 'block';
+};
+
+if (localStorage.getItem('time') >= 21 * 60) {
+    nextVideo()
+}
+
+// Démarre la video si clic sur l'image
+
+function click_on_image() {
+
+    playImage.style.display = 'none';
+    pause_button.style.display = 'none';
+    video.style.display = 'block';
+    video.play()
+};
+
+function disapear(elem) {
+    elem.style.visibility = 'hidden';
+    window.location.reload(true);
+};
+
+playImage.addEventListener('click', click_on_image);
+
+pause_button.addEventListener('click', click_on_image);
+
+// Sidebar
+
+toggleButton.addEventListener('click', show);
+
+function show() {
+    sideBar.classList.toggle('active');
+};
+
+const page = document.getElementById('main');
+
+page.addEventListener('click', () => {
+    sideBar.classList.remove('active');
+});
+
+// Change video quand clic sur element liste
+
+const ext = '.mp4'
+
+function change_video(elem) {
+    if (typeof elem === "string") {
+        var txtvideo = elem;
+        for (let i=0; i<liList.length; i++) {
+            if (liList[i].innerHTML == elem) {
+                var saison = (liList[i].parentElement.id);
+            }
+        }
+    } else {
+        var txtvideo = elem.textContent;
+        var saison = elem.parentElement.id;
+        sideBar.classList.toggle('active');
+    }
+    title.innerHTML = txtvideo;
+    localStorage.setItem('title', txtvideo);
+    txtvideo = 'videos/' + saison + '/' + txtvideo + ext;
+    selected.innerHTML = saison;
+    let season = (selected.innerHTML);
+    for (let i = 0; i < ulList.length; i++) {
+        ulList[i].style.display = 'none';
+    }
+    let ulSeason = document.getElementById(season);
+    ulSeason.style.display = 'block';
+    localStorage.setItem('video', txtvideo);
+    video.setAttribute('src', txtvideo);
+    video.load();
+    playImage.style.display = 'block';
+    pause_button.style.display = 'block';
+    video.style.display = 'none';
+    for (let i = 0; i < liList.length; i++) {
+        liList[i].removeAttribute('style');
+    }
+    if (typeof elem == 'string') {
+        for (let i = 0; i < liList.length; i++) {
+            if (liList[i].textContent == elem) {
+                elem = liList[i];
+                break;
+            }
+        }
+    }
+    elem.style.fontWeight = '600';
+    elem.style.color = 'white';
+
+};
+
+function getEpList() {
+    let = epList = [];
+    for (let i = 0; i < liList.length; i++) {
+        epList.push(liList[i].textContent);
+    }
+    return epList;
+}
+
+function getEpWatching() {
+    let thisEp = video.outerHTML;
+    thisEp = containsString("mp4", thisEp.split("/"));
+    thisEp = (thisEp.slice(0, thisEp.search('mp4') - 1));
+    return thisEp;
+}
+
+
+// Enregistre toutes les secondes le temps dans la vidéo
+
+video.addEventListener('playing', () => {
+    let epList = getEpList();
+    let thisEp = getEpWatching();
+    if (epList.indexOf(thisEp) != epList.length - 1) {
+        setInterval(watchVideo, 1000);
+    }
+    else {
+        nextButton.style.visibility = "hidden";
+    }
+});
+
+
+function watchVideo() {
+    localStorage.setItem('time', video.currentTime);
+    if (video.currentTime >= (video.duration - video.duration * .2)) {
+        nextVideo();
+    } else {
+        nextButton.style.visibility = "hidden";
+    }
+}
+
+function nextVideo() {
+    nextButton.style.visibility = "visible";
+}
+
+function containsString(str, list) {
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].search(str) != -1) {
+            return list[i];
+        }
+    }
+    return false;
+}
+
+nextButton.addEventListener('click', next_video);
+
+function next_video() {
+    let = epList = getEpList();
+    let thisEp = getEpWatching();
+    thisEp = (epList.indexOf(thisEp));
+    change_video(epList[thisEp + 1])
+}
+
+for (let i = 0; i < liList.length; i++) {
+    if (liList[i].textContent == title.innerHTML) {
+        liList[i].style.fontWeight = "600";
+        liList[i].style.color = "white";
+    }
+}
+
+
+
+// add source mp4 compatibility by adding document.write source
+// add type video
+// add button ends soon to watch the next (liste des li, détecter on est auquel et mettre +1)
